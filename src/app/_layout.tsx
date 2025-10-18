c0865168ecbcdccf964f1fc8d8f04498ee25822a
lucasdevfull <lucassilva270805@gmail.com>
2025-10-18 15:24:01 -0300

    configurando cache

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
+import { QueryClient } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+import { createAsyncStoragePersister } from '@tanstack/query-async-storage-persister';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5, // cache fresh por 5 minutos
+      //cacheTime: 1000 * 60 * 60, // cache expira em 1 hora se não usado
+    },
+  },
+});
+
+// Cria o persister usando AsyncStorage
+const persister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+});
 
 export default function Layout() {
-  return <Stack />;
+  return (
+  <PersistQueryClientProvider client={queryClient} persistOptions={{ persister }}>
+    <Stack />
+  </PersistQueryClientProvider>
+);
 }
